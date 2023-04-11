8b4b0e86c3fe47791e29eb763a3b2ed86741693f
gitshubham45 <shubham16290@gmail.com>
2023-04-11 19:49:15 +0530

    level 5 security with passport session

--- app.js
+++ app.js
@@ -3,24 +3,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const LocalStrategy = require('passport-local').Strategy;
 
-mongoose.connect('mongodb://127.0.0.1:27017/usersDB');
-
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
-});
-
-const User = mongoose.model('User', userSchema);
-
-const saltRounds = 10;
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: "Our little  secret.",
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://127.0.0.1:27017/usersDB');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', userSchema);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -34,39 +50,67 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
-app.post('/register', async (req, res) => {
+
+app.get('/secrets', (req, res) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-        const newUser = new User({
-            email: req.body.username,
-            password: hashedPassword
-        });
-        await newUser.save();
-        res.render('secrets');
-    } catch (err) {
-        console.log(err);
-        res.send(err);
+        console.log(req.isAuthenticated());
+        if (req.isAuthenticated()) {
+            res.render('secrets');
+        } else {
+            res.render('login');
+        }
+    } catch (error) {
+        console.log(error);
     }
 });
 
-app.post('/login', async (req, res) => {
+
+app.post('/register', async (req, res) => {
     try {
-        const user = await User.findOne({ email: req.body.username });
-        if (!user) {
-            res.render('home', { error: 'Invalid email or password.' });
-            return;
-        }
-        const isMatch = await bcrypt.compare(req.body.password, user.password);
-        if (isMatch) {
-            res.render('secrets');
-        } else {
-            res.render('home', { error: 'Invalid email or password.' });
-        }
-    } catch (err) {
-        console.error(err);
-        res.send(err);
+        const user = new User({ username: req.body.username });
+        await User.register(user, req.body.password);
+        req.logIn(user, function (err) {
+            if (err) {
+                console.error(err);
+                res.redirect('/register');
+            } else {
+                console.log(req.isAuthenticated()); 
+                res.redirect('/secrets');
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.redirect('/register');
     }
 });
+
+
+app.post('/login', async (req, res) => {
+
+    try {
+        const user = new User({ username: req.body.username, password: req.body.password });
+        req.logIn(user, (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                res.redirect('/secrets');
+            }
+        });
+    } catch (err) {
+        console.error(err);
+    }
+
+});
+
+app.get('/logout', (req, res) => {
+    req.logout(function(err) {
+        if (err) {
+            console.error(err);
+        }
+        res.redirect('/');
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
